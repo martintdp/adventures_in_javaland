fbf72e94a4b8630e4e1dffbb3bb016e241c17be1
MTdP <martin.d.toledo@gmail.com>
2020-12-23 12:00:49 +0100

    Add Dropwizard HelloWorld app

---
+++ hello-world/src/main/java/edu/aij/MavenToBazel/HelloWorld/HelloWorldConfiguration.java
@@ -0,0 +1,10 @@
+package edu.aij.MavenToBazel.HelloWorld;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class HelloWorldConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
